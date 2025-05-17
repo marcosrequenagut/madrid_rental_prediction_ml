bf6c92316445d78c2376cab1a6ab99afe05a66e3
marcosrequenagut <marcosrequenagtu@gmail.com>
2025-05-17 12:45:06 +0200

    Add directory fastapi_api

---
+++ src/fastapi_api/app.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI(
+    title = "Predict Rental Prices in Madrid",
+    description = "A RESTful API for predicting property prices in Madrid based on various features.",
+    version="0.1.0",
+)
+
+@app.get(
+    "/",
+    response_class=HTMLResponse,
+    summary="Beautiful front page for the data service",
+    description="This is the front page of the data service. It provides a welcome message and a link to the API documentation.",
+    tags=["front-page"],
+)
+async def root_page():
+    return """<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Madrid Rental Price Prediction API</title>
+    </head>
+    <body>
+        <h1>Welcome to the Madrid Rental Price Prediction API</h1>
+        <p>This service provides rental price predictions for properties in Madrid based on their features.</p>
+        <p>Check out the API documentation <a href="/docs">here</a> to learn how to use the available endpoints.</p>
+        <p>Visit our project repository on GitHub <a href="https://github.com/marcosrequenagut/madrid_rental_prediction_ml">here</a>.</p>
+    </body>
+</html>"""
