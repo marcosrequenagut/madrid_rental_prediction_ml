44bbe1af0a6789d8e35dbd7097748812a8708ccd
marcosrequenagut <marcosrequenagut@gmail.com>
2025-05-25 12:27:04 +0200

    Create functions to extract and calculate model metrics

--- utils/utils.py
+++ utils/utils.py
@@ -1,4 +1,35 @@
 import mlflow
+import numpy as np
+
+from sklearn.metrics import (
+    mean_absolute_error, mean_squared_error,
+    r2_score, explained_variance_score,
+    mean_absolute_percentage_error
+)
 
 def setup_mlflow():
     mlflow.set_tracking_uri("http://127.0.0.1:5000")
+
+# ------------------------
+# METRICS
+# ------------------------
+
+def calculate_metrics(y_true, y_pred):
+    mse = mean_squared_error(y_true, y_pred)
+    return {
+        'mae': mean_absolute_error(y_true, y_pred),
+        'mse': mse,
+        'rmse': np.sqrt(mse),
+        'r2': r2_score(y_true, y_pred),
+        'mape': mean_absolute_percentage_error(y_true, y_pred),
+        'evs': explained_variance_score(y_true, y_pred)
+    }
+
+def extract_cv_metrics(results, best_idx):
+    return {
+        'mae_train': -results['mean_test_mae'][best_idx],
+        'mse_train': -results['mean_test_mse'][best_idx],
+        'rmse_train': -results['mean_test_rmse'][best_idx],
+        'mape_train': -results['mean_test_mape'][best_idx],
+        'evs_train': results['mean_test_evs'][best_idx]
+    }
