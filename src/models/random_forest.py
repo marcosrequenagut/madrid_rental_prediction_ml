b3f5bf3eb67cce83cf54899c2075a7de9818eba2
marcosrequenagut <marcosrequenagut@gmail.com>
2025-05-25 12:26:11 +0200

    Add metrics

--- src/models/random_forest.py
+++ src/models/random_forest.py
@@ -5,11 +5,10 @@ import numpy as np
 
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import GridSearchCV
-from sklearn.metrics import mean_absolute_error, r2_score, mean_squared_error
-from utils.utils import setup_mlflow
+from sklearn.metrics import mean_absolute_error, r2_score, mean_squared_error, explained_variance_score, make_scorer, mean_absolute_percentage_error
+from utils.utils import setup_mlflow, calculate_metrics, extract_cv_metrics
 
 setup_mlflow()
-print("Tracking URI:", mlflow.get_tracking_uri())
 
 def train_and_log_random_forest_regressor(X_train, X_test, y_train, y_test):
     # Initial configuration
@@ -17,6 +16,16 @@ def train_and_log_random_forest_regressor(X_train, X_test, y_train, y_test):
     model_name = "random_forest_regressor"
 
     run_name = f"{model_name}_{int(time.time())}"
+
+    # Define custom scorers
+    scoring = {
+        'mae': make_scorer(mean_absolute_error, greater_is_better=False),
+        'mse': make_scorer(mean_squared_error, greater_is_better=False),
+        'rmse': make_scorer(lambda y, y_pred: np.sqrt(mean_squared_error(y, y_pred)), greater_is_better=False),
+        'mape': make_scorer(mean_absolute_percentage_error, greater_is_better=False),
+        'r2': make_scorer(r2_score),
+        'evs': make_scorer(explained_variance_score)
+    }
 
     # Save the results of the model
     with mlflow.start_run(run_name=run_name):
@@ -36,41 +45,42 @@ def train_and_log_random_forest_regressor(X_train, X_test, y_train, y_test):
 
         # Define the cross-validation
         rf_cv = GridSearchCV(estimator=rf, param_grid=grid,
-                             scoring='neg_mean_absolute_error',
-                             cv = 5, n_jobs=-1)
+                             scoring=scoring,
+                             refit='r2',
+                             cv = 5, n_jobs=-1,
+                             return_train_score=True)
 
         # Train the model
         rf_cv.fit(X_train, y_train)
 
         # Prediction and metrics on training
-        y_train_pred = rf_cv.predict(X_train)
-        r2_train = r2_score(y_train, y_train_pred)
-        mae_train = mean_absolute_error(y_train, y_train_pred)
+        r2_train = rf_cv.best_score_
+        best_idx = rf_cv.best_index_
+        results = rf_cv.cv_results_
+
+        metrics_train = extract_cv_metrics(results, best_idx)
 
         # Predictions and Metrics on Test
         y_pred = rf_cv.predict(X_test)
-        mae = mean_absolute_error(y_test, y_pred)
-        r2 = r2_score(y_test, y_pred)
-        mse = mean_squared_error(y_test, y_pred)
-        rmse = np.sqrt(mse)
+        metrics_test = calculate_metrics(y_test, y_pred)
+
+        # Best hyperparams of the model
         best_params = rf_cv.best_params_
 
         # Log with MlFlow
         mlflow.log_param("best_hyperparameters", best_params)
-        mlflow.log_metric("mae_test", mae)
-        mlflow.log_metric("r2_test", r2)
-        mlflow.log_metric("mse_test", mse)
-        mlflow.log_metric("rmse_test", rmse)
-        mlflow.log_metric("mae_train", mae_train)
-        mlflow.log_metric("r2_train", r2_train)
+        for metric, value in metrics_test.items():
+            mlflow.log_metric(f"{metric}_test", value)
+        for metric, value in metrics_train.items():
+            mlflow.log_metric(f"{metric}_train", value)
+
 
         # Save the model
-        mlflow.sklearn.log_model(rf_cv.best_estimator_, "modelo_linear_regressor")
+        mlflow.sklearn.log_model(rf_cv.best_estimator_, "random_forest_regressor")
 
-        print(f"Linear Regressor MAE: {mae:.2f}")
-        print(f"KNN MSE: {mse:.2f}")
-        print(f"KNN RMSE: {rmse:.2f}")
-        print(f"Linear Regressor R2: {r2:.2f}")
-        print(f"Linear Regressor Best Params: {best_params}")
+        print("Show the r^2 for Random Forest Regressor:")
+        print(f"R2 on test: {metrics_test['r2']:.2f}")
+        print(f"R2 on training: {r2_train:.2f}")
+        print(f"Best Params: {best_params}")
 
         return rf_cv.best_estimator_
