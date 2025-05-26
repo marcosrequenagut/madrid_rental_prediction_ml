c587107bfae2008951325524f20a44722698b6c1
marcosrequenagut <marcosrequenagtu@gmail.com>
2025-05-26 10:51:34 +0200

    Add metric logging and MLflow tracking for XGBoost Regressor

--- src/models/xgboost_regressor.py
+++ src/models/xgboost_regressor.py
@@ -6,10 +6,9 @@ import numpy as np
 from xgboost import XGBRegressor
 from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import mean_absolute_error, r2_score, mean_squared_error
-from utils.utils import setup_mlflow
 
-setup_mlflow()
-print("Tracking URI:", mlflow.get_tracking_uri())
+from utils.utils import get_regression_scorers, extract_cv_metrics, calculate_metrics
+
 
 def train_and_log_xgboost_regressor(X_train, X_test, y_train, y_test):
     # Initial configuration
@@ -17,6 +16,9 @@ def train_and_log_xgboost_regressor(X_train, X_test, y_train, y_test):
 
     # Name of the model which will be saved
     run_name = f"{model_name}_{int(time.time())}"
+
+    # Define custom scorers
+    scoring = get_regression_scorers()
 
     with mlflow.start_run(run_name=run_name):
         # Define the grid of hyperparameters
@@ -42,41 +44,41 @@ def train_and_log_xgboost_regressor(X_train, X_test, y_train, y_test):
 
         # Cross-validation
         xgb_cv = GridSearchCV(estimator=xgb, param_grid=grid,
-                              scoring='neg_mean_absolute_error',
-                              cv=3, n_jobs=-1)
+                              scoring=scoring,
+                              refit='r2',
+                              cv=3, n_jobs=-1,
+                              return_train_score=True)
 
         # Train
         xgb_cv.fit(X_train, y_train)
 
         # Prediction and metrics on training
-        y_train_pred = xgb_cv.predict(X_train)
-        r2_train = r2_score(y_train, y_train_pred)
-        mae_train = mean_absolute_error(y_train, y_train_pred)
+        r2_train = xgb_cv.best_score_
+        best_idx = xgb_cv.best_index_
+        results = xgb_cv.cv_results_
 
-        # Predictions and Metrics on test
+        metrics_train = extract_cv_metrics(results, best_idx)
+
+        # Predictions and Metrics on Test with the best model
         y_pred = xgb_cv.predict(X_test)
-        r2 = r2_score(y_test, y_pred)
-        mae = mean_absolute_error(y_test, y_pred)
-        mse = mean_squared_error(y_test, y_pred)
-        rmse = np.sqrt(mse)
+        metrics_test = calculate_metrics(y_test, y_pred)
+
+        # Best hyperparams of the model
         best_params = xgb_cv.best_params_
 
         # Log with MlFlow
         mlflow.log_param("best_hyperparameters", best_params)
-        mlflow.log_metric("mae_test", mae)
-        mlflow.log_metric("r2_test", r2)
-        mlflow.log_metric("mse_test", mse)
-        mlflow.log_metric("rmse_test", rmse)
-        mlflow.log_metric("mae_train", mae_train)
-        mlflow.log_metric("r2_train", r2_train)
+        for metric, value in metrics_test.items():
+            mlflow.log_metric(f"{metric}_test", value)
+        for metric, value in metrics_train.items():
+            mlflow.log_metric(metric, value)
 
         # Save the model
-        mlflow.sklearn.log_model(xgb_cv.best_estimator_, "model_xgboost_regressor")
+        mlflow.sklearn.log_model(xgb_cv.best_estimator_, "random_forest_regressor")
 
-        print(f"XGBoost Regressor MAE: {mae:.2f}")
-        print(f"KNN MSE: {mse:.2f}")
-        print(f"KNN RMSE: {rmse:.2f}")
-        print(f"XGBoost Regressor R2: {r2:.2f}")
-        print(f"XGBoost Regressor Best Params: {best_params}")
+        print("Show the r^2 for XGBoost Regressor:")
+        print(f"R2 on test: {metrics_test['r2']:.2f}")
+        print(f"R2 on training: {r2_train:.2f}")
+        print(f"Best Params: {best_params}")
 
         return xgb_cv.best_estimator_
