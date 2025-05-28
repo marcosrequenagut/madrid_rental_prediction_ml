e5399ce7a5a404d7d3a2ddb2f5c6a86613035683
marcosrequenagut <marcosrequenagtu@gmail.com>
2025-05-28 16:54:50 +0200

    Add PropertyFeatures Pydantic model for prediction input validation

---
+++ app/routes/PropertyFeatures.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel
+
+class PropertyFeatures(BaseModel):
+    constructed_area: float
+    has_terrace: int
+    is_parkingspace_included: int
+    number_of_rooms: int
+    number_of_bathrooms: int
+    has_swimming_pool: int
+    is_top_floor: int
+    distance_to_city_center: float
+    distance_to_city_metro: float
+    distance_to_city_castellana: float
+    constructed_year: int
+    floorclean: int
