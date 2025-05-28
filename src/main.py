8eeea9107d2de1f74340ee18237e290800fd5b70
marcosrequenagut <marcosrequenagtu@gmail.com>
2025-05-28 17:03:10 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,14 +1,17 @@
 from src.data_processing.data_preprocessing import load_and_preproces_data
+from src.models.ensemble_voting_regressor import train_ensemble_model
 from src.models.knn import train_and_log_knn
 from src.models.linear_regressor import train_and_log_linear_regressor
+from src.models.random_forest import train_and_log_random_forest_regressor
+from src.models.svm_regressor import train_and_log_svm_regressor
 from src.models.xgboost_regressor import train_and_log_xgboost_regressor
 
 def main():
     # Path where data is stored
     URL = r'C:\Users\34651\Desktop\MASTER\TFM\Data\EDA_Madrid_SCALED.csv'
 
-    # This variable represents the fraction of the original dataset used to create the test set"
-    test_size_fraction = 0.7
+    # This variable represents the fraction of the original dataset used to create the test set
+    test_size_fraction = 0.8
 
     # Obtain the train and test datasets
     X_train, X_test, y_train, y_test = load_and_preproces_data(URL, test_size_fraction)
@@ -17,10 +20,10 @@ def main():
     #train_and_log_knn(X_train, X_test, y_train, y_test)
 
     # Train and predict with Linear Regressor
-    train_and_log_linear_regressor(X_train, X_test, y_train, y_test)
+    #train_and_log_linear_regressor(X_train, X_test, y_train, y_test)
 
     # Train and predict with Random Forest
-    #train_and_log_random_forest_regressor(X_train, X_test, y_train, y_test)
+    train_and_log_random_forest_regressor(X_train, X_test, y_train, y_test)
 
     # Train and Predict with XGBoost Regressor
     #train_and_log_xgboost_regressor(X_train, X_test, y_train, y_test)
