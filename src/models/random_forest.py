8d8078a7f6079c571881ac1c9b22b082a605a717
marcosrequenagut <marcosrequenagtu@gmail.com>
2025-05-28 17:02:41 +0200

    Update metrics in random_forest.py

--- src/models/random_forest.py
+++ src/models/random_forest.py
@@ -1,14 +1,21 @@
 import mlflow
 import mlflow.sklearn
 import time
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import GridSearchCV
 
-from utils.utils import calculate_metrics, extract_cv_metrics, get_regression_scorers
+from utils.utils import calculate_metrics, extract_cv_metrics, get_regression_scorers, \
+    show_tree_model_feature_importance, save_model
+
 
 def train_and_log_random_forest_regressor(X_train, X_test, y_train, y_test):
     # Initial configuration
+    mlflow.set_tracking_uri("http://127.0.0.1:5000")
+
     model_name = "random_forest_regressor"
 
     run_name = f"{model_name}_{int(time.time())}"
@@ -42,6 +49,10 @@ def train_and_log_random_forest_regressor(X_train, X_test, y_train, y_test):
         # Train the model using cross-validation
         rf_cv.fit(X_train, y_train)
 
+        # Important features
+        feature_names = X_train.columns
+        show_tree_model_feature_importance(rf_cv.best_estimator_, feature_names)
+
         # Prediction and metrics on training
         r2_train = rf_cv.best_score_
         best_idx = rf_cv.best_index_
@@ -62,14 +73,15 @@ def train_and_log_random_forest_regressor(X_train, X_test, y_train, y_test):
             mlflow.log_metric(f"{metric}_test", value)
         for metric, value in metrics_train.items():
             mlflow.log_metric(metric, value)
+        mlflow.log_metric("r2_train", r2_train)
 
-
-        # Save the model
-        mlflow.sklearn.log_model(rf_cv.best_estimator_, "random_forest_regressor")
+        # Save the model into the model in mlflow
+        save_model(rf_cv.best_estimator_, model_name)
 
         print("Show the r^2 for Random Forest Regressor:")
         print(f"R2 on test: {metrics_test['r2']:.2f}")
         print(f"R2 on training: {r2_train:.2f}")
         print(f"Best Params: {best_params}")
+
 
         return rf_cv.best_estimator_
