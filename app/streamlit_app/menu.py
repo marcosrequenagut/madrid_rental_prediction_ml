4b0cfcf0af5cff2a983f44c4ad5d7dee73e71624
marcosrequenagut <marcosrequenagtu@gmail.com>
2025-07-21 17:47:43 +0200

    Rename page titles for clarity and consistency; remove obsolete Maps.py file

--- app/streamlit_app/menu.py
+++ app/streamlit_app/menu.py
@@ -3,7 +3,6 @@ import streamlit as st
 def menu():
     # Determine if a user is logged in or not, then show the correct
     # navigation menu
-    st.sidebar.page_link("pages/dashboard.py", label="Estimate My Rent")
-    st.sidebar.page_link("pages/map_all_properties.py", label="Explore Rent Prices")
-    st.sidebar.page_link("pages/map_properties_by_district.py", label="Explore Similar Properties")
-    st.sidebar.page_link("pages/Maps.py", label="Map of Madrid")
+    st.sidebar.page_link("pages/dashboard.py", label="Property Price Prediction")
+    st.sidebar.page_link("pages/map_all_properties.py", label="Madrid Property Price Heatmap")
+    st.sidebar.page_link("pages/map_properties_by_district.py", label="EExplore Similar Properties & Compare Market Prices")
